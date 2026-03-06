a22bc7a9d975af171ad6a5910f3ef1b6c151d5fa
devara-g <defarahermawan@gmail.com>
2026-03-06 21:25:06 +0700

    floating

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include 'components/header.php'; ?>
 
 <section class="hero">
-    <div class="floating-shapes">
+
         <div class="shape shape-1"></div>
         <div class="shape shape-2"></div>
         <div class="shape shape-3"></div>
